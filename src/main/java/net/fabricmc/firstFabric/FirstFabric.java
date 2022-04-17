2a1565f765a7f981bdce77d48de16cd9ac95f03e
joch3em <57416031+joch2em@users.noreply.github.com>
2022-04-17 19:17:19 +0200

    backup

--- src/main/java/net/fabricmc/firstFabric/FirstFabric.java
+++ src/main/java/net/fabricmc/firstFabric/FirstFabric.java
@@ -7,7 +7,10 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.texture.TextureStitcher;
+import net.minecraft.client.util.math.Vector3d;
 import net.minecraft.datafixer.fix.ItemNameFix;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.screen.slot.Slot;
@@ -18,10 +21,12 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.*;
 
 import java.util.List;
 
@@ -34,7 +39,10 @@ public class FirstFabric implements ModInitializer {
 			new Identifier("firstfabric", "general"),
 			() -> new ItemStack(Blocks.COBBLESTONE));
 
-	public static final TestItem FABRIC_ITEM = new TestItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(16).group(FirstFabric.ITEM_GROUP));
+	public static final TestItem FABRIC_ITEM =
+			new TestItem(new FabricItemSettings().group(ItemGroup.MISC).maxCount(16).group(FirstFabric.ITEM_GROUP));
+
+
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("firstfabric");
 
@@ -54,15 +62,20 @@ public class FirstFabric implements ModInitializer {
 		}
 
 		@Override
-		public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-			playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
-			return TypedActionResult.success(playerEntity.getStackInHand(hand));
-		}
-
-		@Override
 		public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
 			tooltip.add( new TranslatableText("item.firstFabric.test_item.tooltip") );
 			tooltip.add( new TranslatableText("item.firstFabric.test_item.tooltip_red").formatted(Formatting.RED) );
+		}
+
+		@Override
+		public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand)
+		{
+			var stack = user.getStackInHand(hand);
+
+			user.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+
+			stack.decrement(1);
+			return TypedActionResult.success(stack);
 		}
 	}
 }
